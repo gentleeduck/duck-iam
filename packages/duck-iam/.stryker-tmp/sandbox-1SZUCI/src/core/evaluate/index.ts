76eee07ee31dcb2b01d4e85ad6a92542f62f2e21
wildduck2 <wezonaser50@gmail.com>
2026-09-01 12:06:54 +0300

    chore(iam): untrack leftover .stryker-tmp sandboxes

--- packages/duck-iam/.stryker-tmp/sandbox-1SZUCI/src/core/evaluate/index.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-export { evaluate, evaluateFast, evaluatePolicy, evaluatePolicyFast } from './evaluate'
-export { indexPolicy } from './evaluate.libs'
-export type { Evaluate } from './evaluate.types'
